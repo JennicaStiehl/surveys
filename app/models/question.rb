a36a70c5845385d34a8fd761010245b68b766a9f
JennicaStiehl <jennica.stiehl@gmail.com>
2019-05-27 10:21:55 -0600

    adds left outer join to the q and a endpoint

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,7 +5,7 @@ class Question < ActiveRecord::Base
   validates_presence_of :question
 
   def self.get_q_and_a(ids)
-    joins(:choices)
+    left_outer_joins(:choices)
     .select(:id, :question)
     .select('choices.id as choice_id, choices.choice, choices.correct')
     .where("questions.id in (#{ids})")
